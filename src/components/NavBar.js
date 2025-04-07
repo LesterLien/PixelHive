e28be0f493d54f66081408bf9ecfe888e110077c
Lester Lien <lesterlien11@gmail.com>
2025-04-06 19:47:12 -0700

    fixed footer and navbar, added more cases for missing game data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,16 +18,22 @@ function NavBar() {
                 <Nav className="nav-text">
                     <Nav.Link as={Link} to="/games">Games</Nav.Link>
                     <Nav.Link as={Link} to="/favorites">Favorites</Nav.Link>
-                    <Nav.Link as={Link} to="/wishlist">Wishlist</Nav.Link>
                 </Nav>
-            <Nav className="ms-auto user"><BsFillPersonFill className='icon'/>
-                <NavDropdown title="User" id="basic-nav-dropdown">
-                    <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/account">Account</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item as={Link} to="/contact">Contact Us</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
+                <Nav className='nav-dropdown-other'>
+                    <NavDropdown title="Other" id="basic-nav-dropdown">
+                        <NavDropdown.Item as={Link} to="/shops">Shops</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item as={Link} to="/contact">Contact Us</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav className="ms-auto nav-dropdown-user"><BsFillPersonFill className='icon'/>
+                    <NavDropdown title="User" id="basic-nav-dropdown">
+                        <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/account">Account</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item as={Link} to="/wishlist">Wishlist</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
             </Navbar.Collapse>
         </Navbar>
     );
