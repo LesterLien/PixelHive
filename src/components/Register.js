c52bd5795c3781eca96818f43057a59092b0a14e
Lester Lien <lesterlien11@gmail.com>
2025-04-09 21:15:10 -0700

    login and register frontend to database connection

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,32 +1,83 @@
 import '../styles/Register.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import axios from "axios";
 
 function Register(){
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+      try {
+        const response = await axios.post('http://localhost:8000/register', {
+          username,
+          password,
+        });
+
+        if (response.status === 200) {
+          navigate('/login'); 
+        }
+      } catch (error) {
+        if (error.response && error.response.data && error.response.data.error) {
+          setErrorMessage(error.response.data.error);
+        } else {
+          setErrorMessage('Something went wrong!');
+        }
+      }
+    };
+
     return (
-        <div className="registerPage-page-container">
-          <div className="registerPage-login-box">
-            <div className="registerPage-login-form-section">
-              <h2>Register</h2>
-              <form>
-                <div className="registerPage-form-group">
-                  <label htmlFor="username">Name</label>
-                  <input type="username" id="username" placeholder="Enter your username" />
-                </div>
-    
-                <div className="registerPage-form-group">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" id="password" placeholder="Enter your password" />
-                </div>
-    
-                <button type="submit" className="registerPage-submit-button">Register</button>
-              </form>
-    
-              <div className="registerPage-signup-link">
-                <p>Already have an account? <Link to="/login">Sign in</Link></p>
-              </div>
+      <div className="registerPage-page-container">
+      <div className="registerPage-login-box">
+        <div className="registerPage-login-form-section">
+          <h2>Register</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="registerPage-form-group">
+              <label htmlFor="username">Name</label>
+              <input
+                type="text"
+                id="username"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                  setErrorMessage('');
+                }}
+              />
             </div>
+
+            <div className="registerPage-form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value)
+                  setErrorMessage('');
+                }}
+              />
+            </div>
+
+            <button type="submit" className="registerPage-submit-button">Register</button>
+          </form>
+
+          {errorMessage && (
+              <p className="registerPage-error-message">{errorMessage}</p>
+          )}
+
+          <div className="registerPage-signup-link">
+            <p>Already have an account? <Link to="/login">Sign in</Link></p>
           </div>
         </div>
-      );
-    }
+      </div>
+    </div>
+  );
+}
 export default Register;
